4a9b7f5ec7ef78a6a1c861b47c4aadf2d4889008
Salil Sinai Cuncoliencar <salil_cuncoliencar@intuit.com>
2018-10-05 15:20:42 +0530

    Spring AOP leaning

---
+++ SpringAop/src/org/salil/learning2/aspect/LoggingAspect.java
@@ -0,0 +1,13 @@
+package org.salil.learning2.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
+public class LoggingAspect {
+
+    @Before("execution(public String getName())")
+    public void LoggingAdvice(){
+        System.out.println("Advice Run. Get method called");
+    }
+}
